2c47c17163cfd5facbcb8127169d6f6ba8c04697
Евгений Субачёв <subachev@reg.ru>
2017-09-02 21:02:54 +0400

    Добавил parse_otrs_error_log.pl

---
+++ parse_otrs_error_log.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+use Modern::Perl;
+
+my $amount; # Cчётчик
+
+open FILEWORK, "<", "otrs_error.log" or die "Ошибка открытия файла otrs_error.log: $!\n";
+
+while (<FILEWORK>) {
+	if (/ .* Error .* Kernel::System::Ticket::TicketPermission /x) {
+		if (/ .* Aug .* 6 .* 1 [0-7] : .* : .* 2017 .* /x) {
+			$amount++;
+		}
+		elsif (/ .* Aug .* 6 .* 18:00:00 .* 2017 .* /x) {
+			$amount++;
+		}
+	}
+}
+close FILEWORK;
+
+say $amount;
